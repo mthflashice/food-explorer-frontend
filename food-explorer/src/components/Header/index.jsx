18218ba3f603b675a219b5130341bc7608444a68
mthflashice <matheuslino@kerix.com.br>
2023-11-06 15:19:29 -0300

    styled from Header

---
+++ food-explorer/src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import {FiSearch, FiLogOut} from 'react-icons/fi'
+import {Container, Brand, Search, Logout} from './styles'
+ 
+import {  } from 
+import {  } from 
+import {  } from 
+import {  } from 
