0fa9e09debf6a27e23c0dd270e4da2b1069f9db5
mthflashice <matheuslino@kerix.com.br>
2023-11-08 10:17:48 -0300

    theme in Main

--- food-explorer/src/main.jsx
+++ food-explorer/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
+import {SignIn} from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <SignIn/>
+    </ThemeProvider>
     <App />
   </React.StrictMode>,
 )
