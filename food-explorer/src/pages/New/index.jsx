ac76e5c94bef9d4a0d2154574abc0bf82cccfb99
mthflashice <matheuslino@kerix.com.br>
2023-11-09 11:36:48 -0300

    create component Section

--- food-explorer/src/pages/New/index.jsx
+++ food-explorer/src/pages/New/index.jsx
@@ -1,13 +1,17 @@
 import { Container } from "./styles";
 import {Header} from '../../components/Header'
 import { Textarea } from "../../components/Textarea";
+import { Section } from "../../components/Section";
 
 export function New(){
     return(
         <Container>
             <Header isAdmin />
             
+            <Section title='Descrição'>
             <Textarea placeholder ='Fale brevemente sobre o prato, seus ingredientes e composição'/>
+            </Section>
+            
         </Container>
     )
 
