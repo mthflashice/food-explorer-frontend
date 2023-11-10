094a0267cf4510dcbbb145015981c63f609437d6
mthflashice <matheuslino@kerix.com.br>
2023-11-10 11:39:30 -0300

    created FootItem

--- food-explorer/src/pages/New/index.jsx
+++ food-explorer/src/pages/New/index.jsx
@@ -2,11 +2,17 @@ import { Container } from "./styles";
 import {Header} from '../../components/Header'
 import { Textarea } from "../../components/Textarea";
 import { Section } from "../../components/Section";
+import { FoodItem } from "../../components/FoodItem";
 
 export function New(){
     return(
         <Container>
             <Header isAdmin />
+
+            <Section title='Ingredientes'>
+                <FoodItem value = 'Pão Naan'/>
+                <FoodItem isNew placeholder = 'Adicionar'/>
+            </Section>
             
             <Section title='Descrição'>
             <Textarea placeholder ='Fale brevemente sobre o prato, seus ingredientes e composição'/>
