e6ab3bad5e3ef2b0d6822d49eb86370c4a58d166
mthflashice <matheuslino@kerix.com.br>
2023-11-13 14:02:31 -0300

    created component ButtonText

--- food-explorer/src/pages/Dish/index.jsx
+++ food-explorer/src/pages/Dish/index.jsx
@@ -2,11 +2,17 @@ import { Container } from "./styles";
 import { Header } from "../../components/Header";
 import {Footer} from '../../components/Footer'
 import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
+import {RxCaretLeft} from 'react-icons/rx'
 
 export function Dish(){
     return (
         <Container>
             <Header/>
+            <ButtonText>
+                <RxCaretLeft/>
+                voltar
+            </ButtonText>
 
             <Tag title='alface'/>
             <Tag title='cebola'/>
