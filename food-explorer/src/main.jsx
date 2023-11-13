e9b810fa59d84c86c219251079acf637738f835a
mthflashice <matheuslino@kerix.com.br>
2023-11-13 14:09:04 -0300

    created Page Home

--- food-explorer/src/main.jsx
+++ food-explorer/src/main.jsx
@@ -6,12 +6,13 @@ import theme from './styles/theme'
 import {SignIn} from './pages/SignIn'
 import {New} from './pages/New'
 import { Dish } from './pages/Dish'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Dish/>
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>,
 )
