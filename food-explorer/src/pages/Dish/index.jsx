6baad46851503cbd5594f4863e8521aa277899d7
mthflashice <matheuslino@kerix.com.br>
2023-11-13 12:41:01 -0300

    created component Tag

--- food-explorer/src/pages/Dish/index.jsx
+++ food-explorer/src/pages/Dish/index.jsx
@@ -1,11 +1,15 @@
 import { Container } from "./styles";
 import { Header } from "../../components/Header";
-import  {Footer} from '../../components/Footer'
+import {Footer} from '../../components/Footer'
+import { Tag } from "../../components/Tag";
 
 export function Dish(){
     return (
         <Container>
             <Header/>
+
+            <Tag title='alface'/>
+            <Tag title='cebola'/>
 
             <Footer/>
         </Container>
