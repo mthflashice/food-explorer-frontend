bacf6d7d80587c5260a1f6a7c6d9c6d980bd4e20
mthflashice <matheuslino@kerix.com.br>
2023-11-14 10:23:27 -0300

    created NumberPicker

---
+++ food-explorer/src/components/NumberPicker/index.jsx
@@ -0,0 +1,12 @@
+import {FiMinus, FiPlus} from 'react-icons/fi'
+import { Container } from './styles'
+
+export function NumberPicker(){
+    return(
+        <Container>
+            <FiMinus/>
+            <span>01</span>
+            <FiPlus/>
+        </Container>
+    );
+}
