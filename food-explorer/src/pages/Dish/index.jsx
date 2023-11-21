8a1f20532f1fc52bfc4b5e7998a090a186a2e8b8
mthflashice <matheuslino@kerix.com.br>
2023-11-21 10:41:53 -0300

    Finalized  page Dish

--- food-explorer/src/pages/Dish/index.jsx
+++ food-explorer/src/pages/Dish/index.jsx
@@ -1,23 +1,56 @@
-import { Container } from "./styles";
+import { Container,Content } from "./styles";
+import {RxCaretLeft} from 'react-icons/rx'
 import { Header } from "../../components/Header";
 import {Footer} from '../../components/Footer'
 import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
-import {RxCaretLeft} from 'react-icons/rx'
+import { NumberPicker } from "../../components/NumberPicker";
+import { Button } from "../../components/Button";
 
-export function Dish(){
+export function Dish(isAdmin){
     return (
         <Container>
-            <Header/>
+            {isAdmin?<Header isAdmin/>: <Header/>}
+           
+           <main>
+            <header>
             <ButtonText>
                 <RxCaretLeft/>
                 voltar
             </ButtonText>
+            </header>
+         
+        <Content>
+            <img src="../../../src/assets/salada-ravanello.png.png" alt="Salada Ravanello"/>
 
-            <Tag title='alface'/>
-            <Tag title='cebola'/>
+            <div>
+                <h1>Salada Ravenello</h1>
+                <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
 
+                <section>
+                    <Tag title='alface'/>
+                    <Tag title='cebola'/>
+                    <Tag title='pão naan'/>
+                    <Tag title='pepino'/>
+                    <Tag title='rabanete'/>
+                    <Tag title='tomate'/>
+                </section>
+
+                <div className="buttons">
+                    {isAdmin?
+                    <Button title='Editar prato' className='edit'/>:
+                    <>
+                    <NumberPicker/>
+                    <Button title='Incluir R$ 25,00'  className='include'/>
+                    </>}
+                </div>
+            </div>
+        </Content>
+           
+
+                </main>
             <Footer/>
         </Container>
+       
     );
 }
