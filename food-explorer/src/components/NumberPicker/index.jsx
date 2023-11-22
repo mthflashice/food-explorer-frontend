22b1caccd632fc1443f62d17c820a8d722da69a0
mthflashice <matheuslino@kerix.com.br>
2023-11-22 11:55:20 -0300

    upload NumberPicker

--- food-explorer/src/components/NumberPicker/index.jsx
+++ food-explorer/src/components/NumberPicker/index.jsx
@@ -1,12 +1,25 @@
 import {FiMinus, FiPlus} from 'react-icons/fi'
 import { Container } from './styles'
+import { useState } from 'react';
 
 export function NumberPicker(){
+    const [number,setNumber] = useState(1);
+
+    const incrementNumber = ()=>{
+        setNumber(number+1);
+    };
+
+    const decrementNumber = ()=>{
+        if(number > 1){
+        setNumber(number -1);
+    }
+};
+
     return(
         <Container>
-            <FiMinus/>
-            <span>01</span>
-            <FiPlus/>
+            <button onClick={decrementNumber}><FiMinus/></button>
+            <span>{number <10 ? `0${number}`:number}</span>
+            <button onClick={incrementNumber}> <FiPlus/></button>
         </Container>
     );
 }
