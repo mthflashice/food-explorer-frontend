f193f9d621d7d683aa13c13a58fef8feec3dcc5e
mthflashice <matheuslino@kerix.com.br>
2023-11-22 10:23:02 -0300

    Button Update

--- food-explorer/src/components/Button/index.jsx
+++ food-explorer/src/components/Button/index.jsx
@@ -1,7 +1,11 @@
 import {Container} from './styles'
 import {TbReceipt} from 'react-icons/tb'
+import {useMediaQuery} from 'react-responsive'
 
 export function Button({title, loading=false,isCustomer, ...rest}){
+
+    const isDesktop =useMediaQuery({minWidth:1024});
+    
     return(
         <Container
             type= 'button'
@@ -10,7 +14,8 @@ export function Button({title, loading=false,isCustomer, ...rest}){
             >
              {isCustomer &&  <TbReceipt size={32}/>}
              {loading? 'carregando':title}
-             {isCustomer && <span>{rest.orderCount}</span>}
+             {isCustomer && <span>{isDesktop?`(${rest.orderCount})`:
+             rest.orderCount}</span>}
         </Container>
     );
 };
