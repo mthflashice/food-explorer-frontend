832df6644ab15e9e3a3a83552072673f27fadf23
mthflashice <matheuslino@kerix.com.br>
2023-11-24 11:48:04 -0300

    upload page Dish

--- food-explorer/src/components/ButtonText/styles.js
+++ food-explorer/src/components/ButtonText/styles.js
@@ -5,11 +5,15 @@ export const Container = styled.button `
     border: none;
 
     font-family: 'Poppins', sans-serif;
-    font-weight: 700;
+    font-weight: 500;
     font-size: 24px;
     line-height: 140%;
 
     color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+    @media (min-width: 1024px) {
+    font-weight: 700;
+  }
 
     display: flex;
     align-items: center;
--- food-explorer/src/pages/Dish/index.jsx
+++ food-explorer/src/pages/Dish/index.jsx
@@ -1,16 +1,26 @@
 import { Container,Content } from "./styles";
 import {RxCaretLeft} from 'react-icons/rx'
+import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
 import { Header } from "../../components/Header";
 import {Footer} from '../../components/Footer'
 import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
 import { NumberPicker } from "../../components/NumberPicker";
 import { Button } from "../../components/Button";
+import {Menu} from '../../components/Menu' 
 
 export function Dish(isAdmin){
+    const isDesktop = useMediaQuery({ minWidth: 1024 });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <Container>
-            {isAdmin?<Header isAdmin/>: <Header/>}
+            {!isDesktop && 
+        <Menu isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      }
+
+      <Header isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
            
            <main>
             <header>
@@ -41,7 +51,10 @@ export function Dish(isAdmin){
                     <Button title='Editar prato' className='edit'/>:
                     <>
                     <NumberPicker/>
-                    <Button title='Incluir R$ 25,00'  className='include'/>
+                    <Button 
+                    title={isDesktop ? 'Incluir ∙ R$ 25,00' : 'pedir . R$ 25,00'}
+                    className='include'
+                    isCustomer={!isDesktop}/>
                     </>}
                 </div>
             </div>
