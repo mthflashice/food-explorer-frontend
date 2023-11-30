2b518b88e4366b6f15f39397cd731d9ec9ccac6d
mthflashice <matheuslino@kerix.com.br>
2023-11-30 12:30:32 -0300

    add Register  in SignUp

--- food-explorer/src/pages/SignUp/index.jsx
+++ food-explorer/src/pages/SignUp/index.jsx
@@ -1,13 +1,40 @@
 import {Container, Form, Brand } from './styles'
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate  } from 'react-router-dom'; 
+import {api} from '../../services/api'
 import {Button} from '../../components/Button'
 import { Input } from '../../components/Input'
 import { Section } from '../../components/Section';
 import brand from '../../assets/brand.svg'
+import { useState } from 'react';
 
 
 
 export function SignUp (){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert('Preencha todos os campos!');
+    }
+
+    api
+    .post('/users', { name, email, password })
+    .then(() => {
+      alert('Usuário cadastrado com sucesso!');
+      navigate(-1);
+    })
+    .catch((error) => {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert('Não foi possível cadastrar.');
+      }
+    });
+}
     return (
         <Container>
             <Brand>
@@ -21,6 +48,7 @@ export function SignUp (){
                     <Input
                     placeholder = 'Exemplo: Maria da Silva'
                     type = 'text'
+                    onChange={e => setName(e.target.value)}
                     />
                 </Section>
 
@@ -28,6 +56,7 @@ export function SignUp (){
                     <Input
                     placeholder = 'Exemplo: exemplo@exemplo.com.br'
                     type = 'text'
+                    onChange={e => setEmail(e.target.value)}
                     />
                 </Section>
 
@@ -35,10 +64,11 @@ export function SignUp (){
                     <Input
                     placeholder = 'No mínimo 6 caracteres'
                     type = 'password'
+                    onChange={e => setPassword(e.target.value)}
                     />
                 </Section>
 
-                <Button title='Criar Conta'/>
+                <Button title='Criar Conta' onClick={handleSignUp}/>
 
                 <Link to ='/'>
                     Já tenho uma conta
