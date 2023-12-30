69665d1a053547f578f3f64143bf47fe85d5fca5
mthflashice <matheuslino@kerix.com.br>
2023-12-29 23:31:12 -0300

    handle new and signOut

--- food-explorer/src/components/Menu/index.jsx
+++ food-explorer/src/components/Menu/index.jsx
@@ -1,24 +1,37 @@
 import { Container } from "./styles";
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
 
 import { Header } from '../../components/Header';
 import { Search } from '../Search';
 import { ButtonText } from '../ButtonText';
 
-export function Menu ({ isAdmin, isMenuOpen, setIsMenuOpen,setSearch }){
+export function Menu({ isAdmin, isMenuOpen, setIsMenuOpen, setSearch, isDisabled }) {
+    const { signOut } = useAuth();
+    const navigate = useNavigate();
+  
+    function handleNew() {
+      navigate("/new");
+    }
+  
+    function handleSignOut() {
+      navigate("/");
+      signOut();
+    }
     return(
         <Container isMenuOpen={isMenuOpen}>
             <Header isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
 
             <main>
-                <Search setSearch={setSearch}/>
+                <Search isDisabled={isDisabled} setSearch={setSearch} />
 
-                {isAdmin &&
-                <ButtonText>
+                {isAdmin ? (
+                <ButtonText onClick={handleNew}>
                     Novo Prato
                     </ButtonText>
-                    }
+                    ) : null}
 
-                    <ButtonText>
+                    <ButtonText onClick={handleSignOut}>
                         Sair
                     </ButtonText>
             </main>
