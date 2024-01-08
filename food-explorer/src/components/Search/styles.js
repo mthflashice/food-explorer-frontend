6b3c485c1e24a0cbd93a12234f8bb850003e9978
mthflashice <matheuslino@kerix.com.br>
2024-01-08 11:41:59 -0300

    fix styles Search

--- food-explorer/src/components/Search/styles.js
+++ food-explorer/src/components/Search/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     padding-left:1.4rem;
+    flex-grow: 1;
     border-radius: 0.5rem;
     
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_500};
