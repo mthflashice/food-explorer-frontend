9bb2c726dbc8cd54adf1a41c8229e1cc5febd71b
mthflashice <matheuslino@kerix.com.br>
2024-01-18 12:12:30 -0300

    add ';'

--- food-explorer/src/routes/index.jsx
+++ food-explorer/src/routes/index.jsx
@@ -3,14 +3,15 @@ import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
 import { useAuth } from '../hooks/auth';
 
-export function Routes (){
+export function Routes() {
     const { user, isUserAuthenticated } = useAuth();
     const isAdmin = user ? user.is_admin : false;
 
     const userAuthenticated = isUserAuthenticated();
-    return(
+    
+    return (
         <BrowserRouter>
          {(user && userAuthenticated) ? <AppRoutes isAdmin={isAdmin} /> : <AuthRoutes />}
         </BrowserRouter>
-    )
+    );
 }
