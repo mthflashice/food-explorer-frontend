33841864facf1568d8d0b7bc8678a5696787446b
mthflashice <matheuslino@kerix.com.br>
2024-01-18 11:32:03 -0300

    spaces

--- food-explorer/src/pages/Home/styles.js
+++ food-explorer/src/pages/Home/styles.js
@@ -1,28 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-
-    display: grid;
-    grid-template-rows: 11.4rem auto 7.7rem;
-    grid-template-areas:
+  width: 100%;
+  display: grid;
+  grid-template-rows: 11.4rem auto 7.7rem;
+  grid-template-areas:
     'header'
     'content'
     'footer';
 
-    >main{
-        grid-area: content;
-        justify-self: center;
+  > main {
+    grid-area: content;
+    justify-self: center;
 
-        width: 100vw;
+    width: 100vw;
+
+    > div {
+      width: calc(100% - 2.4rem);
+      margin: 4.4rem 0 2.5rem 2.4rem;
     
-
-    >div{
-        width: calc(100% - 2.4rem);
-        margin: 4.4rem 0 2.5rem 2.4rem;
-    
-
-    >header{
+      header {
         height: 12rem;
         margin-inline: 1.2rem 1.6rem;
 
@@ -31,53 +28,53 @@ export const Container = styled.div`
         align-items: flex-end;
 
         background: linear-gradient(
-            180deg, 
+          180deg, 
           ${({ theme }) => theme.COLORS.GRADIENT_100} 0%, 
           ${({ theme }) => theme.COLORS.GRADIENT_200} 100%
         );
         border-radius: 0.3rem;
-
+        
         position: relative;
 
-        img{
-            width:19.1rem;
+        img {
+          width: 19.1rem;
 
-            position: absolute;
-            left: -3rem;
-            bottom: 0;
+          position: absolute;
+          left: -3rem;
+          bottom: 0;
         }
 
-        div{
-            width: 20.2rem;
-            position: absolute;
-            top: 3.6rem;
-            right: 2.1rem;
+        div {
+          width: 20.2rem;
+          position: absolute;
+          top: 3.6rem;
+          right: 2.1rem;
 
-            color: ${({ theme }) => theme.COLORS.GRAY_200};
-        
+          color: ${({ theme }) => theme.COLORS.GRAY_200};
 
-        h1{
+          h1 {
             font-family: 'Poppins', sans-serif;
             font-weight: 600;
             font-size: 1.8rem;
             line-height: 140%;
 
             margin-bottom: 0.3rem;
-        }
+          }
 
-        p{
+          p {
             font-size: 1.2rem;
             line-height: 140%;
+          }
         }
-     }
+      }
     }
- }
-}
-@media (min-width: 1024px) {
+  }
+
+  @media (min-width: 1024px) {
     height: 100vh;
     grid-template-rows: 9.6rem auto 7.7rem;
 
-    >main{
+    > main {
       overflow-y: auto;
 
       ::-webkit-scrollbar {
@@ -127,36 +124,37 @@ export const Container = styled.div`
     }
   }
 `;
+
 export const Content = styled.div`
-   display: flex;
-   flex-direction: column;
-   gap: 2.4rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
 
-   margin-top: 6.2rem;
+  margin-top: 6.2rem;
 
-   >section >h2{
-     font-family: 'Poppins', sans-serif;
-     font-weight: 500;
-     font-size: 1.8rem;
-     line-height: 140%;
+  section > h2 {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 1.8rem;
+    line-height: 140%;
 
-     color: ${({ theme }) => theme.COLORS.GRAY_200};
-     margin-bottom: 2.4rem;
-   }
-
-   swiper-container {
+    color: ${({ theme }) => theme.COLORS.GRAY_200};
+    margin-bottom: 2.4rem;
+  }
+  
+  swiper-container {
     height: 29.2rem;
   }
 
-   swiper-slide{
+  swiper-slide {
     max-width: 21rem;
-   }
+  }
 
-   @media (min-width: 1024px) {
+  @media (min-width: 1024px) {
     gap: 4.8rem;
 
-    section > h2{
-        font-size: 3.2rem;
+    section > h2 {
+      font-size: 3.2rem;
     }
 
     swiper-container {
@@ -164,7 +162,7 @@ export const Content = styled.div`
     }
 
     swiper-container::before,
-    swiper-container::after{
+    swiper-container::after {
       content: '';
       position: absolute;
       top: 0;
@@ -172,8 +170,8 @@ export const Content = styled.div`
       width: 22.4rem;
       z-index: 2;
       pointer-events: none;
-      
     }
+
     swiper-container::before {
       left: 0;
       background: linear-gradient(
@@ -182,6 +180,7 @@ export const Content = styled.div`
         transparent 100%
       );
     }
+
     swiper-container::after {
       right: 0;
       background: linear-gradient(
@@ -191,8 +190,8 @@ export const Content = styled.div`
       );
     }
 
-    swiper-slide{
-    max-width: 30.4rem;
-   }
-   }
+    swiper-slide {
+      max-width: 30.4rem;
+    }
+  }
 `;
