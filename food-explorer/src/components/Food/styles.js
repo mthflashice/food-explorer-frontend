5aabf5d4e94514d6ff1a56d4a5d63520e25be8df
mthflashice <matheuslino@kerix.com.br>
2024-01-23 10:49:08 -0300

    fix food min-width in 1024px

--- food-explorer/src/components/Food/styles.js
+++ food-explorer/src/components/Food/styles.js
@@ -29,7 +29,7 @@ export const Container =styled.div`
 
     >img{
         max-width: 88px;
-        margin-top:${({isAdmin})=>isAdmin?'4.6rem' : 'none'};   
+        margin-top: ${({ isAdmin }) => isAdmin ? '4.6rem' : 'none'};   
         
         cursor: pointer;
     }
@@ -37,7 +37,8 @@ export const Container =styled.div`
     >span{
         line-height: 100%;
         color: ${({ theme }) => theme.COLORS.LIGHT_BLUE};
-        margin-top:${({isAdmin})=>isAdmin?'4.6rem' : 'none'};     
+        
+        margin-bottom:${({isAdmin})=>isAdmin?'4.6rem' : 'none'};     
     }
 
     @media (min-width: 1024px) {
@@ -51,8 +52,8 @@ export const Container =styled.div`
 
         >img{
             max-width: 17.6rem;
-            margin-top:${({isAdmin})=>isAdmin?'3.2rem' : 'none'};     
-        }
+            margin-top: ${({ isAdmin }) => isAdmin ? '3.2rem' : 'none'};
+    }
         >p{
             font-size: 1.4rem;
             line-height: 160%;
@@ -62,35 +63,36 @@ export const Container =styled.div`
             overflow: hidden;
         }
 
-        >span{
-        font-size: 3.2rem;
-        line-height: 160%;
-        margin-top:${({isAdmin})=>isAdmin?'3.2rem' : 'none'}; 
+    > span {
+      font-size: 3.2rem;
+      line-height: 160%;
+
+      margin-bottom: ${({ isAdmin }) => isAdmin ? '3.2rem' : 'none'};
     }
-    }
+  }
 `;
 
 export const Title = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+  > h2 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    text-align: center;
+
     width: 100%;
-    display: flex;
-    align-items: center;
-    color: ${({ theme }) => theme.COLORS.GRAY_200};
+    
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 
-    >h2{
-        font-family: 'Poppins', sans-serif;
-        font-weight: 500;
-        font-size: 1.4rem;
-        line-height: 2.4rem;
-        text-align: center;
-
-        width: 100%;
-
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-
-    @media(min-width: 1024){
+    @media(min-width: 1024px){
         >h2{
         font-weight: 700;
         font-size: 2.4rem;
@@ -100,22 +102,23 @@ export const Title = styled.div`
 `;
 
 export const Order = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1.6rem;
+  width: 100%;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
 
-    >button{
-        padding: 0.4rem 2.4rem;
+  > button {
+    padding: 0.4rem 2.4rem;
+  }
+
+  @media (min-width: 1024px) {
+    width: fit-content;
+    flex-direction: row;
+
+    > button {
+      padding: 1.2rem 2.4rem;
     }
-
-    @media(min-width: 1024){
-        width: fit-content;
-        flex-direction: row;
-
-        >button{
-        padding: 1.2rem 2.4rem;
-    }
-   }
+  }
 `;
