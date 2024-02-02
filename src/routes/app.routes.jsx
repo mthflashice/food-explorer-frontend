ee2bcf140b24f28b26c67ba73f704abde35c032b
mthflashice <matheuslino@kerix.com.br>
2024-02-02 19:54:55 -0300

    routes order => myOrder

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -5,7 +5,7 @@ import { Edit } from './../pages/Edit';
 import { Dish } from './../pages/Dish'
 import { Home } from './../pages/Home'
 import { Favorites } from '../pages/Favorites';
-import { Orders } from '../pages/Orders';
+import { Orders } from '../pages/MyOrders';
 
 export function AppRoutes ({ $Isadmin }) {
     return(
